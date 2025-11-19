6e0f5b71f1af3d2eb9fd41d8a56cbd0cfda78b78
Reece, Andrew <andrew.reece@student.swic.edu>
2025-11-19 13:52:35 -0600

    feat: Display results on page

--- src/form-practice.js
+++ src/form-practice.js
@@ -1,11 +1,19 @@
 const form = document.querySelector("form");
+const result = document.querySelector("#result");
 
-// Same pattern as 'clicks', but for 'submit' events.
 form.addEventListener("submit", (event) => {
-  // No, browser! WE will handle this. Stop your default behavior.
   event.preventDefault();
   const formData = new FormData(form);
   const data = Object.fromEntries(formData);
-
-  console.log(data);
+  // Pass the data object to the Results function to get HTML! 🚀
+  result.innerHTML = Results(data);
 });
+
+function Results(data) {
+  return `<ul class="space-y-1 text-sm">
+        ${Object.entries(data)
+          .map(([k, v]) => `<li><strong>${k}:</strong> ${v}</li>`)
+          .join("")}
+    </ul>   
+    `;
+}
