74420b0ea93171bde13cf165357508898ec60cf4
Reece, Andrew <andrew.reece@student.swic.edu>
2025-11-19 21:33:31 -0600

    Display and Code Functioning - autocomplete broke

--- src/main.js
+++ src/main.js
@@ -1,12 +1,35 @@
 import "./style.css";
 
-const form = document.querySelector("form");
+const REQUIRED = ["name", "email", "password", "checker"];
 
-// Same pattern as 'clicks', but for 'submit' events.
+const form = document.querySelector("form");
+const errorDisplay = document.getElementById('error');
+// CSS attribute selector.
+const submitBtn = form.querySelector('[type="submit"]');
+
+submitBtn.disabled = true; // Start disabled
+
+form.addEventListener("input", () => {
+  // Does EVERY form input field have a non-empty value?
+  REQUIRED.every((field) => form[field].value.trim() !== "")
+    ? (submitBtn.disabled = false) // If yes, enable button
+    : (submitBtn.disabled = true); // If no, disable button
+});
 form.addEventListener("submit", (event) => {
-  // No, browser! WE will handle this. Stop your default behavior.
   event.preventDefault();
+  const matches = REQUIRED.every((field) => form[field].value.trim() !== "");
+  const passwordChecker = form.password.value === form.checker.value;
   const formData = new FormData(form);
   const data = Object.fromEntries(formData);
-  console.log(data);
+  if (submitBtn.disabled = !(matches && passwordChecker))
+  {
+    errorDisplay.textContent = `Passwords Do Not Match`;
+  }
+  else 
+  {
+    console.log(data);
+    errorDisplay.textContent = ``;
+    form.reset();
+    submitBtn.disabled = true;
+  }
 });
